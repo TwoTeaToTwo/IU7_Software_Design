bed39254f596ff4c5a10990aa09b11ffc8725f65
Averyanov Dmitriy <averyanovda@student.bmstu.ru>
2025-06-14 01:10:19 +0300

    add db/postgres config files

---
+++ src/database/postgres/schema.ts
@@ -0,0 +1,29 @@
+import {
+	integer,
+	pgTable,
+	primaryKey,
+	serial,
+	text,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+	id: serial().primaryKey().notNull(),
+	login: text(),
+	password: text(),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+	id: serial("id").primaryKey(),
+	url: text("url").notNull().unique(),
+	title: text("title"),
+	platform: text("platform").notNull(),
+});
+
+export const usersHaveSubscriptions = pgTable("user_have_subscriptions", {
+	user_id: integer("user_id").references(() => users.id).notNull(),
+	subscription_id: integer("subscription_id").references(() =>
+		subscriptions.id
+	).notNull(),
+}, (table) => [{
+	pk: primaryKey({ columns: [table.user_id, table.subscription_id] }),
+}]);
