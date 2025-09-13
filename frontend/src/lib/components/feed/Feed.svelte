55d37338846ec40cb6e283256a58c89bbcad79c1
twoteatotwo <dima-av@yandex.ru>
2025-09-13 23:19:48 +0300

    add update feed button

--- frontend/src/lib/components/feed/Feed.svelte
+++ frontend/src/lib/components/feed/Feed.svelte
@@ -7,57 +7,43 @@
 	import { playingPodcast } from '../../stores/Store';
 	import Player from '../Player.svelte';
 	import SubscriptionsPanel from './SubscriptionsPanel.svelte';
+	import Bubbles from '../Bubbles.svelte';
+	import PopUp from '../PopUp.svelte';
 
-	const feedContent = $state(new Array<PodcastViewModel>());
-	// const feedSize = 10;
-	// Добавить кнопку получить и убрать код снизу
-	// $effect(() => {
-	// 	authController.getAccessToken().then((accessToken) => {
-	// 		FeedViewModel.getFeedContent(feedSize, accessToken!, feedContent).then(() => {
-	// 			console.log('feed is updated;');
-	// 		});
-	// 	});
-	// });
-	const podcast1: PodcastViewModel = {
-		title: 'Meow meow meow meow meow meow meow meow meow meow meow meow meow meow',
-		platform: 'youtube',
-		duration_s: 94,
-		relevance: 'test',
-		url: 'https://youtu.be/AtPrjYp75uA?si=K2VGXcbtTGhTXdjc',
-		durationText: '1:34',
-		relevanceText: '4 Feb, 2024'
+	let showMessage = $state(false);
+	let popUpMessage = $state('');
+	let messageTitle = $state('');
+	const messageHandler = (msg: string, title: string) => {
+		messageTitle = title;
+		popUpMessage = msg;
+		showMessage = true;
+		setTimeout(() => {
+			showMessage = false;
+		}, 2000);
 	};
-	const podcast2: PodcastViewModel = {
-		title: 'Wow',
-		platform: 'youtube',
-		duration_s: 4,
-		relevance: 'test',
-		url: 'https://youtu.be/BnTdfA5aTpY?si=Z7JWNB43D6g13NZC',
-		durationText: '0:04',
-		relevanceText: '15 Feb, 2023'
-	};
-	feedContent.push(podcast1);
-	feedContent.push(podcast2);
+
+	let feedContent = $state(new Array<PodcastViewModel>());
+	let isUpdateButtonHovered = $state(false);
+	const feedSize = 10;
+	const updateFeed = () => {
+			messageHandler("Updating feed", "FEED");
+			authController.getAccessToken().then((accessToken) => {
+			FeedViewModel.getFeedContent(feedSize, accessToken!, feedContent).then(() => {
+				messageHandler("Feed is updated!", "FEED");
+			});
+		});
+	}
 </script>
 
-<div class="bubble bubble--1"></div>
-<div class="bubble bubble--2"></div>
-<div class="bubble bubble--3"></div>
-<div class="bubble bubble--4"></div>
-<div class="bubble bubble--5"></div>
-<div class="bubble bubble--6"></div>
-<div class="bubble bubble--7"></div>
-<div class="bubble bubble--8"></div>
-<div class="bubble bubble--9"></div>
-<div class="bubble bubble--10"></div>
-<div class="bubble bubble--11"></div>
-<div class="bubble bubble--12"></div>
+<PopUp isOpen={showMessage} message={popUpMessage} title={messageTitle} />
+<Bubbles />
 <SubscriptionsPanel />
 <div class="container">
 	<div class="search">
 		<Search />
 	</div>
-	<div class="component-title">Feed</div>
+	<button class="button_component" onclick={() => updateFeed()} onmouseenter={() => isUpdateButtonHovered = true} onmouseleave={() => isUpdateButtonHovered = false}
+>{isUpdateButtonHovered ? "Update Feed" : "Feed"}</button>
 	<div class="feed-content">
 		{#each feedContent as podcast}
 			<Podcast
@@ -110,7 +96,7 @@
 		margin-bottom: 20px;
 	}
 
-	.component-title {
+	.button_component {
 		color: #ffffff;
 		font-family: 'Nunito Sans', sans-serif;
 		font-size: 72pt;
@@ -118,138 +104,16 @@
 		margin-top: 0;
 		margin-bottom: auto;
 		position: relative;
+		background: none;
+		border: none;
+		transition: all 0.2s ease;
 	}
 
-	.bubble {
-		width: 30px;
-		height: 30px;
-		border-radius: 100%;
-		position: absolute;
-		background-color: white;
-		bottom: -30px;
-		opacity: 0.2;
-		animation:
-			bubble 15s ease-in-out infinite,
-			sideWays 4s ease-in-out infinite alternate;
+	.button_component:hover {
+		font-size: 80pt;
 	}
 
-	@keyframes bubble {
-		0% {
-			transform: translateY(0%);
-			opacity: 0.06;
-		}
-		100% {
-			transform: translateY(-120vh);
-		}
-	}
-
-	@keyframes sideWays {
-		0% {
-			margin-left: 0px;
-		}
-		100% {
-			margin-left: 200px;
-		}
-	}
-
-	.bubble--1 {
-		left: 10%;
-		animation-delay: 0.5s;
-		animation-duration: 16s;
-		opacity: 0.2;
-	}
-
-	.bubble--2 {
-		width: 15px;
-		height: 15px;
-		left: 40%;
-		animation-delay: 1s;
-		animation-duration: 10s;
-		opacity: 0.1;
-	}
-
-	.bubble--3 {
-		width: 10px;
-		height: 10px;
-		left: 30%;
-		animation-delay: 5s;
-		animation-duration: 20s;
-		opacity: 0.3;
-	}
-
-	.bubble--4 {
-		width: 25px;
-		height: 25px;
-		left: 40%;
-		animation-delay: 8s;
-		animation-duration: 17s;
-		opacity: 0.2;
-	}
-
-	.bubble--5 {
-		width: 30px;
-		height: 30px;
-		left: 60%;
-		animation-delay: 10s;
-		animation-duration: 15s;
-		opacity: 0.1;
-	}
-
-	.bubble--6 {
-		width: 10px;
-		height: 10px;
-		left: 80%;
-		animation-delay: 3s;
-		animation-duration: 30s;
-		opacity: 0.4;
-	}
-
-	.bubble--7 {
-		width: 15px;
-		height: 15px;
-		left: 90%;
-		animation-delay: -7s;
-		animation-duration: 25s;
-		opacity: 0.3;
-	}
-
-	.bubble--9 {
-		width: 20px;
-		height: 20px;
-		left: 50%;
-		bottom: 30px;
-		animation-delay: -5s;
-		animation-duration: 19s;
-		opacity: 0.2;
-	}
-
-	.bubble--10 {
-		width: 40px;
-		height: 40px;
-		left: 30%;
-		bottom: 30px;
-		animation-delay: -21s;
-		animation-duration: 16s;
-		opacity: 0.3;
-	}
-
-	.bubble--11 {
-		width: 30px;
-		height: 30px;
-		left: 60%;
-		bottom: 30px;
-		animation-delay: -13.75s;
-		animation-duration: 20s;
-		opacity: 0.3;
-	}
-
-	.bubble--11 {
-		width: 25px;
-		height: 25px;
-		left: 90%;
-		bottom: 30px;
-		animation-delay: -10.5s;
-		animation-duration: 19s;
-		opacity: 0.3;
+	.button_component:active {
+		color: #00ccfe;
 	}
 </style>
