fab8ab6d6e566498fa0d440c13fe87534cbd9905
twoteatotwo <dima-av@yandex.ru>
2025-09-13 21:35:43 +0300

    add water loading on starting cite

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,8 +5,8 @@
 </script>
 
 {#await authController.responseAccessToken()}
-	<!-- TODO -->
-	<span>Loading</span>
+	<div class="text text-in">Loading</div>
+	<div class="text text-out">Loading</div>
 {:then}
 	{#if $isLogged === false}
 		<Login />
@@ -14,3 +14,56 @@
 		<Feed />
 	{/if}
 {/await}
+
+<style>
+	.text {
+		font-family: 'Nunito Sans', sans-serif;
+		color: #fff;
+		font-size: 8em;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.text-in {
+		color: transparent;
+		-webkit-text-stroke: 2px #00ccfe;
+	}
+
+	.text-out {
+		color: #00ccfe;
+		animation: animate 4s ease-in-out infinite;
+	}
+
+	@keyframes animate {
+		0%,
+		100% {
+			clip-path: polygon(
+				0% 45%,
+				16% 44%,
+				33% 50%,
+				54% 60%,
+				70% 61%,
+				84% 59%,
+				100% 52%,
+				100% 100%,
+				0% 100%
+			);
+		}
+
+		50% {
+			clip-path: polygon(
+				0% 60%,
+				15% 65%,
+				34% 66%,
+				51% 62%,
+				67% 50%,
+				84% 45%,
+				100% 46%,
+				100% 100%,
+				0% 100%
+			);
+		}
+	}
+</style>
