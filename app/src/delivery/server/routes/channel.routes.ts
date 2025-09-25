8ff6f327d7e978db5f15e59a05f0a2819b0967ac
twoteatotwo <dima-av@yandex.ru>
2025-09-25 23:03:18 +0300

    add channel.routes.ts openapi info for subscription crud

--- app/src/delivery/server/routes/channel.routes.ts
+++ app/src/delivery/server/routes/channel.routes.ts
@@ -1,9 +1,18 @@
 import type { FastifyPluginAsync } from "fastify";
 import {
+	getSubscriptionErrorSchema,
+	getSubscriptionOkSchema,
+	getSubscriptionQuerySchema,
 	subscribeErrorSchema,
 	subscribeOkSchema,
 	subscribeSchema,
+	unsubscribeErrorSchema,
 	unsubscribeSchema,
+	updateSubscriptionCreatedSchema,
+	updateSubscriptionErrorSchema,
+	updateSubscriptionSchema,
+	updateSubscriptionTitleErrorSchema,
+	updateSubscriptionTitleSchema,
 } from "../schemas/channel.schemas.ts";
 import { ChannelController } from "../controllers/channel.controller.ts";
 
@@ -32,9 +41,54 @@ export const userChannelRoutes: FastifyPluginAsync = async (app) => {
 			security: [{ bearerAuth: [] }],
 			response: {
 				204: { type: "null" },
-				404: subscribeErrorSchema,
-				500: subscribeErrorSchema,
+				404: unsubscribeErrorSchema,
+				500: unsubscribeErrorSchema,
 			},
 		},
 	}, ChannelController.unsubscribe);
+	app.get("", {
+		preHandler: [app.authenticate],
+		schema: {
+			querystring: getSubscriptionQuerySchema,
+			tags: ["user", "channel"],
+			summary: "get subscription by id",
+			security: [{ bearerAuth: [] }],
+			response: {
+				200: getSubscriptionOkSchema,
+				404: getSubscriptionErrorSchema,
+				500: getSubscriptionErrorSchema,
+			},
+		},
+	}, () => { //TODO
+	});
+	app.put("", {
+		preHandler: [app.authenticate],
+		schema: {
+			body: updateSubscriptionSchema,
+			tags: ["user", "channel"],
+			summary: "update subscription information",
+			security: [{ bearerAuth: [] }],
+			response: {
+				201: updateSubscriptionCreatedSchema,
+				204: { type: "null" },
+				500: updateSubscriptionErrorSchema,
+			},
+		},
+	}, () => { //TODO
+	});
+	app.patch("", {
+		preHandler: [app.authenticate],
+		schema: {
+			body: updateSubscriptionTitleSchema,
+			tags: ["user", "channel"],
+			summary: "update subscription title",
+			security: [{ bearerAuth: [] }],
+			response: {
+				204: { type: "null" },
+				404: updateSubscriptionTitleErrorSchema,
+				500: updateSubscriptionTitleErrorSchema,
+			},
+		},
+	}, () => { //TODO
+	});
 };
--- app/src/delivery/server/schemas/channel.schemas.ts
+++ app/src/delivery/server/schemas/channel.schemas.ts
@@ -23,3 +23,20 @@ export const unsubscribeSchema = Type.Object({
 export type UnsubscribeType = Static<
 	typeof unsubscribeSchema
 >;
+export const unsubscribeErrorSchema = Type.String();
+
+export const getSubscriptionQuerySchema = Type.Object({
+	subscribe_id: Type.Integer({ minimum: 1 }),
+});
+export const getSubscriptionOkSchema = subscribeObjectSchema;
+export const getSubscriptionErrorSchema = Type.String();
+
+export const updateSubscriptionSchema = subscribeObjectSchema;
+export const updateSubscriptionCreatedSchema = subscribeObjectSchema;
+export const updateSubscriptionErrorSchema = Type.String();
+
+export const updateSubscriptionTitleSchema = Type.Object({
+	id: Type.Integer({ minimum: 1 }),
+	title: Type.String(),
+});
+export const updateSubscriptionTitleErrorSchema = Type.String();
