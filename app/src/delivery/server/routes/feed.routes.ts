38ffc88362d11227e74a47b31e5ce7d6941edbd8
twoteatotwo <dima-av@yandex.ru>
2025-09-25 19:17:25 +0300

    add feed.routes.ts openapi info

--- app/src/delivery/server/routes/feed.routes.ts
+++ app/src/delivery/server/routes/feed.routes.ts
@@ -1,11 +1,24 @@
 import type { FastifyPluginAsync } from "fastify";
-import { getFeedContentSchema } from "../schemas/feed.schemas.ts";
+import {
+	getFeedContentErrorSchema,
+	getFeedContentOkSchema,
+	getFeedContentSchema,
+} from "../schemas/feed.schemas.ts";
 import { FeedController } from "../controllers/feed.controller.ts";
 
 export const feedRoutes: FastifyPluginAsync = async (app) => {
 	await Promise.resolve();
 	app.get("/content", {
 		preHandler: [app.authenticate],
-		schema: { querystring: getFeedContentSchema },
+		schema: {
+			querystring: getFeedContentSchema,
+			tags: ["user", "feed"],
+			summary: "get feed content",
+			security: [{ bearerAuth: [] }],
+			response: {
+				200: getFeedContentOkSchema,
+				404: getFeedContentErrorSchema,
+			},
+		},
 	}, FeedController.getFeedContent);
 };
--- app/src/delivery/server/schemas/feed.schemas.ts
+++ app/src/delivery/server/schemas/feed.schemas.ts
@@ -1,4 +1,5 @@
 import { type Static, Type } from "@sinclair/typebox";
+import { podcastSchema } from "./search.schemas.ts";
 
 export const getFeedContentSchema = Type.Object({
 	feed_size: Type.Optional(Type.Integer({ minimum: 1 })),
@@ -6,3 +7,5 @@ export const getFeedContentSchema = Type.Object({
 export type GetFeedContentType = Static<
 	typeof getFeedContentSchema
 >;
+export const getFeedContentOkSchema = Type.Array(podcastSchema);
+export const getFeedContentErrorSchema = Type.String();
