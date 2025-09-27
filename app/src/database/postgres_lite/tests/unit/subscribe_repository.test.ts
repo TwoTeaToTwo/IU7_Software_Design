6398e1a267c90a8bffce54f2b77a671bed9bb6ba
twoteatotwo <dima-av@yandex.ru>
2025-09-27 18:39:34 +0300

    fix some sumscribe_repository tests

--- app/src/database/postgres_lite/tests/unit/subscribe_repository.test.ts
+++ app/src/database/postgres_lite/tests/unit/subscribe_repository.test.ts
@@ -1,36 +1,42 @@
-// import {
-// 	type PostgresLiteDB,
-// 	SubscribeRepository,
-// } from "@podcast/database_postgres_lite";
-// import { SubscribeMother } from "@podcast/core";
-// import { assertEquals } from "@std/assert";
-// import { drizzle } from "drizzle-orm/pglite";
-// import { MigrationHelper, SubscribeHelper } from "../helpers.ts";
-// import { createUInt } from "../../../../core/types.ts";
+import {
+	type PostgresLiteDB,
+	SubscribeRepository,
+} from "@podcast/database_postgres_lite";
+import { SubscribeMother } from "@podcast/core";
+import { assertEquals } from "@std/assert";
+import { drizzle } from "drizzle-orm/pglite";
+import { MigrationHelper } from "../helpers.ts";
 
-// const subscribeMother = new SubscribeMother();
-// let db: PostgresLiteDB;
-// let subscribeHelper: SubscribeHelper;
+const subscribeMother = new SubscribeMother();
+let db: PostgresLiteDB;
 
-// Deno.test.beforeAll(async () => {
-// 	db = drizzle();
-// 	const migrationHelper = new MigrationHelper(db);
-// 	subscribeHelper = new SubscribeHelper(db);
-// 	await migrationHelper.setupTestDb();
-// });
+Deno.test.beforeAll(async () => {
+	db = drizzle();
+	const migrationHelper = new MigrationHelper(db);
+	await migrationHelper.setupTestDb();
+});
 
-// Deno.test("Database: SubscribeRepository: create: add new subscribe", async () => {
-// 	const subscribe = subscribeMother.createYoutubeSubscribe({});
-// 	const subscribeRepository = new SubscribeRepository(db);
+Deno.test("Database: SubscribeRepository: create: add new subscribe", async () => {
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
 
-// 	const result = await subscribeRepository.create(
-// 		subscribe.url,
-// 		subscribe.title,
-// 		subscribe.platform,
-// 	);
+	const result = await subscribeRepository.create(
+		subscribe.url,
+		subscribe.title,
+		subscribe.platform,
+	);
 
-// 	assertEquals(result, subscribe);
-// });
+	assertEquals(result, subscribe);
+});
+
+Deno.test("Database: SubscribeRepository: save: subscribe exists", async () => {
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.save(subscribe);
+
+	assertEquals(result, true);
+});
 
 // Deno.test("Database: SubscribeRepository: findById: subscribe exists", async () => {
 // 	const subscribe = subscribeMother.createYoutubeSubscribe({});
@@ -51,15 +57,6 @@
 // 	const result = await subscribeRepository.findById(subscribe.id);
 
 // 	assertEquals(result, null);
-// });
-
-// Deno.test("Database: SubscribeRepository: save: subscribe exists", async () => {
-// 	const subscribe = subscribeMother.createYoutubeSubscribe({ title: "new" });
-// 	const subscribeRepository = new SubscribeRepository(db);
-
-// 	const result = await subscribeRepository.save(subscribe);
-
-// 	assertEquals(result, true);
 // });
 
 // Deno.test("Database: SubscribeRepository: save: subscribe doesn't exist", async () => {
