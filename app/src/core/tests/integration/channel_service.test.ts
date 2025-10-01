23e3347d88c8d0d0240cdbb2ea86efbda5d93dea
twoteatotwo <dima-av@yandex.ru>
2025-10-01 23:29:49 +0300

    add integration test for channel service core

--- app/src/core/tests/integration/channel_service.test.ts
+++ app/src/core/tests/integration/channel_service.test.ts
@@ -1,18 +1,13 @@
 import { assertEquals } from "@std/assert";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import { ChannelService, SearchService } from "../../mod.ts";
-import type { ISearchStrategy, SearchPlatform } from "../../mod.ts";
 import { SubscribeMother, UserMother } from "../object_mothers.ts";
 import {
 	databaseConfig,
 	fixtures,
 	type PostgresDB,
-	SubscribeManageRepository,
-	SubscribeRepository,
-	UserRepository,
 } from "@podcast/database_postgres";
-import { InMemorySearchStrategy } from "@podcast/in_memory_search";
+import { createChannelService } from "./factory_methods.ts";
 
 const subscribeMother = new SubscribeMother();
 const userMother = new UserMother();
@@ -38,23 +33,7 @@ Deno.test("ChannelService: subscribe: user exists, channel exists", async () => 
 	await fixtures.fillUserTableFixture(db);
 	const subscribe = subscribeMother.createYoutubeSubscribe();
 	const user = userMother.createUser();
-	const subscribeManageRepository = new SubscribeManageRepository(db);
-	const subscribeRepository = new SubscribeRepository(db);
-	const userRepository = new UserRepository(db);
-	const inMemorySearchStrategy: ISearchStrategy =
-		new InMemorySearchStrategy();
-	const searchStrategies = new Map<SearchPlatform, ISearchStrategy>();
-	searchStrategies.set(
-		inMemorySearchStrategy.getPlatform(),
-		inMemorySearchStrategy,
-	);
-	const searchService = new SearchService(searchStrategies);
-	const channelService = new ChannelService(
-		searchService,
-		subscribeManageRepository,
-		userRepository,
-		subscribeRepository,
-	);
+	const channelService = createChannelService(db);
 
 	const result = await channelService.subscribe(
 		user.id,
---
+++ app/src/core/tests/integration/factory_methods.ts
@@ -0,0 +1,30 @@
+import { ChannelService, SearchService } from "../../mod.ts";
+import type { ISearchStrategy, SearchPlatform } from "../../mod.ts";
+import {
+	type PostgresDB,
+	SubscribeManageRepository,
+	SubscribeRepository,
+	UserRepository,
+} from "@podcast/database_postgres";
+import { InMemorySearchStrategy } from "@podcast/in_memory_search";
+
+export const createChannelService = (db: PostgresDB): ChannelService => {
+	const subscribeManageRepository = new SubscribeManageRepository(db);
+	const subscribeRepository = new SubscribeRepository(db);
+	const userRepository = new UserRepository(db);
+	const inMemorySearchStrategy: ISearchStrategy =
+		new InMemorySearchStrategy();
+	const searchStrategies = new Map<SearchPlatform, ISearchStrategy>();
+	searchStrategies.set(
+		inMemorySearchStrategy.getPlatform(),
+		inMemorySearchStrategy,
+	);
+	const searchService = new SearchService(searchStrategies);
+	const channelService = new ChannelService(
+		searchService,
+		subscribeManageRepository,
+		userRepository,
+		subscribeRepository,
+	);
+	return channelService;
+};
