81e4077f498005815460fcd5578049c858d254ca
twoteatotwo <dima-av@yandex.ru>
2025-10-01 22:20:19 +0300

    update integration tests for subscribe_repository postgres

--- app/src/database/postgres/tests/fixtures.ts
+++ app/src/database/postgres/tests/fixtures.ts
@@ -1,10 +1,9 @@
 import type { PostgresDB } from "../mod.ts";
-import { users } from "../schema.ts";
+import { subscriptions, users } from "../schema.ts";
 import { sql } from "drizzle-orm";
-import { UserMother } from "@podcast/core";
+import { SubscribeMother, UserMother } from "@podcast/core";
 
 export const clearUserTableFixture = async (db: PostgresDB): Promise<void> => {
-	// await db.delete(users);
 	await db.execute(
 		sql`TRUNCATE TABLE ${sql.raw("users")} RESTART IDENTITY CASCADE`,
 	);
@@ -14,7 +13,31 @@ export const fillUserTableFixture = async (db: PostgresDB): Promise<void> => {
 	const userMother = new UserMother();
 	const user = userMother.createUser();
 	await db.insert(users).values({
+		id: user.id,
 		login: user.login,
 		password: user.password.password,
+	});
+};
+
+export const clearSubscriptionsTableFixture = async (
+	db: PostgresDB,
+): Promise<void> => {
+	await db.execute(
+		sql`TRUNCATE TABLE ${
+			sql.raw("subscriptions")
+		} RESTART IDENTITY CASCADE`,
+	);
+};
+
+export const fillSubscriptionsTableFixture = async (
+	db: PostgresDB,
+): Promise<void> => {
+	const subscribeMother = new SubscribeMother();
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	await db.insert(subscriptions).values({
+		id: subscribe.id,
+		url: subscribe.url.toString(),
+		title: subscribe.title,
+		platform: subscribe.platform,
 	});
 };
--- app/src/database/postgres/tests/integration/subscribe_repository.integration.test.ts
+++ app/src/database/postgres/tests/integration/subscribe_repository.integration.test.ts
@@ -1,229 +1,113 @@
 import {
-	INJECT_TYPES,
 	type PostgresDB,
 	SubscribeRepository,
 } from "@podcast/database_postgres";
-import { subscriptions } from "../../schema.ts";
-import { createUInt, Subscribe } from "@podcast/core";
-import { Container } from "inversify";
-import { assertEquals } from "jsr:@std/assert";
-import { max } from "npm:drizzle-orm";
-import { drizzle } from "npm:drizzle-orm/node-postgres";
-import { Pool } from "npm:pg";
+import { assertEquals } from "@std/assert";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { SubscribeMother } from "@podcast/core";
+import { databaseConfig } from "../../config.ts";
+import {
+	clearSubscriptionsTableFixture,
+	fillSubscriptionsTableFixture,
+} from "../fixtures.ts";
 
-async function getLastSubscribeId(db: PostgresDB): Promise<number> {
-	const result = await db.select({ value: max(subscriptions.id) }).from(
-		subscriptions,
-	);
-	const record = result[0];
-	if (record !== null && record.value !== null) {
-		return record.value;
-	} else {
-		return 1;
-	}
-}
+const subscribeMother = new SubscribeMother();
+let connectionPool: Pool;
+let db: PostgresDB;
+
+Deno.test.beforeEach(() => {
+	connectionPool = new Pool({
+		connectionString: databaseConfig.testConnectionString,
+	});
+	db = drizzle(connectionPool);
+});
+
+Deno.test.afterEach(async () => {
+	await clearSubscriptionsTableFixture(db);
+	await connectionPool.end();
+});
 
 Deno.test("Database: SubscribeRepository: create: add new subscribe", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.create(
+		subscribe.url,
+		subscribe.title,
+		subscribe.platform,
 	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@IzzyLaif");
-	const title = "IzzyLaif";
-	const platform = "youtube";
-	const result = await repo.create(url, title, platform);
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db)),
-		url,
-		title,
-		platform,
-	);
-	await pool.end();
+
 	assertEquals(result, subscribe);
 });
 
 Deno.test("Database: SubscribeRepository: create: add existing subscribe", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
+	await fillSubscriptionsTableFixture(db);
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.create(
+		subscribe.url,
+		subscribe.title,
+		subscribe.platform,
 	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@IzzyLaif");
-	const title = "IzzyLaif";
-	const platform = "youtube";
-	const result = await repo.create(url, title, platform);
-	await pool.end();
+
 	assertEquals(result, null);
 });
 
 Deno.test("Database: SubscribeRepository: findById: subscribe exists", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@IzzyLaif");
-	const title = "IzzyLaif";
-	const platform = "youtube";
-	const result = await repo.findById(
-		await createUInt(await getLastSubscribeId(db)),
-	);
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db)),
-		url,
-		title,
-		platform,
-	);
-	await pool.end();
+	await fillSubscriptionsTableFixture(db);
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.findById(subscribe.id);
+
 	assertEquals(result, subscribe);
 });
 
 Deno.test("Database: SubscribeRepository: findById: subscribe doesn't exist", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const result = await repo.findById(
-		await createUInt(await getLastSubscribeId(db) + 1),
-	);
-	await pool.end();
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.findById(subscribe.id);
+
 	assertEquals(result, null);
 });
 
 Deno.test("Database: SubscribeRepository: save: subscribe exists", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@izzylie");
-	const title = "IzzyLaif eng";
-	const platform = "youtube";
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db)),
-		url,
-		title,
-		platform,
-	);
-	const result = await repo.save(subscribe);
-	await pool.end();
+	await fillSubscriptionsTableFixture(db);
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.save(subscribe);
+
 	assertEquals(result, true);
 });
 
 Deno.test("Database: SubscribeRepository: save: subscribe doesn't exist", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@izzylie");
-	const title = "IzzyLaif eng";
-	const platform = "youtube";
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db) + 1),
-		url,
-		title,
-		platform,
-	);
-	const result = await repo.save(subscribe);
-	await pool.end();
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.save(subscribe);
+
 	assertEquals(result, false);
 });
 
 Deno.test("Database: SubscribeRepository: delete: subscribe exists", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@izzylie");
-	const title = "IzzyLaif eng";
-	const platform = "youtube";
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db)),
-		url,
-		title,
-		platform,
-	);
-	const result = await repo.delete(subscribe);
-	await pool.end();
+	await fillSubscriptionsTableFixture(db);
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.delete(subscribe);
+
 	assertEquals(result, true);
 });
 
 Deno.test("Database: SubscribeRepository: delete: subscribe doesn't exist", async () => {
-	const test_container = new Container();
-	const pool = new Pool({ connectionString: Deno.env.get("POSTGRES_URL")! });
-	const db = drizzle(pool);
-	test_container.bind(INJECT_TYPES.NodePgDatabase).toConstantValue(
-		db,
-	);
-	test_container.bind(INJECT_TYPES.SubscribeRepository).to(
-		SubscribeRepository,
-	);
-	const repo = test_container.get<SubscribeRepository>(
-		INJECT_TYPES.SubscribeRepository,
-	);
-	const url = new URL("https://www.youtube.com/@izzylie");
-	const title = "IzzyLaif eng";
-	const platform = "youtube";
-	const subscribe = new Subscribe(
-		createUInt(await getLastSubscribeId(db)),
-		url,
-		title,
-		platform,
-	);
-	const result = await repo.delete(subscribe);
-	await pool.end();
+	const subscribe = subscribeMother.createYoutubeSubscribe();
+	const subscribeRepository = new SubscribeRepository(db);
+
+	const result = await subscribeRepository.delete(subscribe);
+
 	assertEquals(result, false);
 });
